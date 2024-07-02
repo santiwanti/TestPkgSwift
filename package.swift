bf446a8837f2b474a678b0959d078d51b0ab5a8e
santiwanti <santi.detord@posteo.net>
2024-07-02 13:49:35 +0200

    first commit

---
+++ package.swift
@@ -0,0 +1,18 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+   name: "Shared",
+   platforms: [
+     .iOS(.v14),
+   ],
+   products: [
+      .library(name: "Shared", targets: ["Shared"])
+   ],
+   targets: [
+      .binaryTarget(
+         name: "Shared",
+         url: "https://github.com/santiwanti/TextXcFramework/blob/main/Shared_xcframework.zip",
+         checksum:"dd77a348e254cb53d31dfc5c48c410db928f14a575d7464f78ac7d91dea48f75")
+   ]
+)
